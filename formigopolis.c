c001925b81d5787ae8e484e4df0650a234c15012
bielbieloliveirag8-eng <bielbieloliveirag8@gmail.com>
2026-06-18 16:34:59 -0300

    Fix type conversion from float to int for caixa count

--- formigopolis.c
+++ formigopolis.c
@@ -2,6 +2,48 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include "formigopolis.h"
+
+void monitor_init(monitor_caixa *mc)
+{
+    pthread_mutex_init(&mc->mutex, NULL);
+
+    mc->caixaOcupado = 0;
+
+    // Inicializa filas e variáveis de condição dinamicamente
+    for (int i = 0; i < QTD_PRIORIDADES; i++)
+    {
+        mc->filaCaixa[i] = 0;
+        pthread_cond_init(&mc->condincaoPorPrioridade[i], NULL);
+    }
+}
+
+void esperar(Pessoa *pessoa, monitor_caixa *mc)
+{
+    pthread_mutex_lock(&mc->mutex);
+    printf("%s está na fila do caixa\n", pessoa->nome);
+    pthread_mutex_unlock(&mc->mutex);
+}
+
+void liberar(Pessoa *pessoa, monitor_caixa *mc)
+{
+    printf("%s vai para casa\n", pessoa->nome);
+}
+
+void atendidoPeloCaixa(Pessoa *pessoa)
+{
+    printf("%s está sendo atendido(a)\n",pessoa->nome);
+}
+
+void vaiEmboraParaCasa(Pessoa *pessoa)
+{
+    printf("%s vai para casa\n",pessoa->nome);
+}
+
+void verificar(Pessoa *pessoa, monitor_caixa *mc)
+{
+    // gerente verifica se ha deadlock
+}
 
 int main(int argc, char const *argv[])
 {
@@ -11,7 +53,6 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    int num_vezes_caixa = atof(argv[1]);
-    
+    int num_vezes_caixa = atoi(argv[1]);
     return 0;
 }
